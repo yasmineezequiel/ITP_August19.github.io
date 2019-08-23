e72f3fa78befedf9032ad9451f7dca7445128d15
yasmine <yasmine.ezequiel@gmail.com>
2019-08-23 15:02:51 +0200

    updated my_group

--- my_group.rb
+++ my_group.rb
@@ -1,13 +1,5 @@
+print person_1 = {name: "Anna", gender: "Female", age:20}
+print person_2 = {name:"Steven", gender: "Male", age:25} 
+print person_3 = {name:"Nick", gender: "Male", age:30} 
 
-19:14 $ irb
-2.6.3 :001 > my_group = ["Anna", "Steven", "Nick"]
- => ["Anna", "Steven", "Nick"] 
-2.6.3 :002 > hash1 = {name: "Anna", gender: "Female", age:20}
- => {:name=>"Anna", :gender=>"Female", :age=>20} 
-2.6.3 :003 > hash2 = {name:"Steven", gender: "Male", age:25}
- => {:name=>"Steven", :gender=>"Male", :age=>25} 
-2.6.3 :004 > hash3 = {name:"Nick", gender: "Male", age:30}
- => {:name=>"Nick", :gender=>"Male", :age=>30} 
-2.6.3 :005 > my_group = [hash1, hash2, hash3]
- => [{:name=>"Anna", :gender=>"Female", :age=>20}, {:name=>"Steven", :gender=>"Male", :age=>25}, {:name=>"Nick", :gender=>"Male", :age=>30}] 
-2.6.3 :006 > 
+
