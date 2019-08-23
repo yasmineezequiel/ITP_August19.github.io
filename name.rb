e79ef1161320d1fa5a450f780b1da45d9ae63be4
yasmine <yasmine.ezequiel@gmail.com>
2019-08-23 14:50:06 +0200

    name.rb updated

--- name.rb
+++ name.rb
@@ -1,14 +1,5 @@
-20:35 $ irb
-2.6.3 :001 > puts "Enter your name "
-Enter your name 
- => nil 
-2.6.3 :002 > name=gets 
-put ("Hello" + name)
- => "put (\"Hello\" + name)\n" 
-2.6.3 :003 > puts "Enter your name: "
-Enter your name: 
- => nil 
-2.6.3 :004 > name = gets 
-put (Hello " + name)
- => "put (Hello \" + name)\n" 
+puts "Enter your name: "
+name = gets
+puts ("Have a great day" + " " + name)
+
  
