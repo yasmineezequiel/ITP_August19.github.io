b30f3f99c4d3c5db8dae59c455f19faa83acdf2d
yasmine <yasmine.ezequiel@gmail.com>
2019-08-23 21:13:23 +0200

    hash_array.rb

---
+++ hash_loops.rb
@@ -0,0 +1,8 @@
+sales = { "Anna" => 20, "Ulrika" => 8, "Jakob" => 3 }
+
+sales.each do |sales|
+    puts sales
+end
+
+
+
--- movies.rb
+++ movies.rb
@@ -1,4 +1,4 @@
-puts films = [
+Movies = [
 { name: "Jaws" , year:1975}, 
 { name: "The Incredibles", year: 2004},
 { name: "Man of Steel", year: 2013},
