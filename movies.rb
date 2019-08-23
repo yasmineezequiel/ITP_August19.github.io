36c0cdcf7f52ce4a2a5d4cfb3def82a456588716
yasmine <yasmine.ezequiel@gmail.com>
2019-08-23 15:09:13 +0200

    the_basics.rb updated

---
+++ movies.rb
@@ -0,0 +1,6 @@
+Movies = [
+{ name: "Jaws" , year:1975}, 
+{ name: "The Incredibles", year: 2004},
+{ name: "Man of Steel", year: 2013},
+{ name: "The Lord of the Rings", year: 2001},
+{ name: "Blow Out", year:1981}]
--- the_basics.rb
+++ the_basics.rb
@@ -1,12 +1,4 @@
-first_name = "John"
-last_name = "Doe"
-print ( first_name + last_name)
+puts "John" + " " + "Doe"
 
-Movies = [
-{ name: "Jaws" , year:1975}, 
-{ name: "The Incredibles", year: 2004},
-{ name: "Man of Steel", year: 2013},
-{ name: "The Lord of the Rings", year: 2001},
-{ name: "Blow Out", year:1981}]
 
 
