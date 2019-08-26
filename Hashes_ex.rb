b5a97d2c06e05eff377bff7ff6e5bcd9be03a6ad
yasmineezequiel <52795566+yasmineezequiel@users.noreply.github.com>
2019-08-26 13:19:04 +0200

    Delete Hashes_ex.rb

--- Hashes_ex.rb
+++
@@ -1,9 +0,0 @@
-family = { 
-    uncles: ["bob", "joe", "steve"],
-    sisters: ["jane", "jill", "beth"],
-    brothers: ["frank", "rob", "david"],
-    auts: ["mary", "sally", "susan"]
-}
-family.each do |immediatefamilymembers|
-end
-
