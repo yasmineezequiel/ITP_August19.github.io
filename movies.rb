f0a4b9ad3f7cb0438556b1fe61e060ec3c322c89
yasmineezequiel <52795566+yasmineezequiel@users.noreply.github.com>
2019-08-26 13:25:24 +0200

    Delete movies.rb

--- movies.rb
+++
@@ -1,6 +0,0 @@
-Movies = [
-{ name: "Jaws" , year:1975}, 
-{ name: "The Incredibles", year: 2004},
-{ name: "Man of Steel", year: 2013},
-{ name: "The Lord of the Rings", year: 2001},
-{ name: "Blow Out", year:1981}]
